a9736f40e6ff9017a7b342dbd97534cb375e40c5
Ali Manouchehri <ali@manouchehri.ca>
2023-06-15 18:28:48 +0000

    Added reference to existing keyvault

--- data_sources.tf
+++ data_sources.tf
@@ -8,15 +8,21 @@ provider "azurerm" {
 data "azurerm_private_dns_zone" "pdns_aks" {
   provider            = azurerm.p
   name                = "privatelink.canadacentral.azmk8s.io"
-  resource_group_name = var.resource_group_name
+  resource_group_name = var.vnet_resource_group_name
 }
 
 data "azurerm_private_dns_zone" "flexpsql" {
   provider            = azurerm.p
   name                = "privatelink.postgres.database.azure.com"
-  resource_group_name = var.resource_group_name
+  resource_group_name = var.vnet_resource_group_name
 }
 
 #endregion Private DNS zones
+
+# a pre-existing key vault
+data "azurerm_key_vault" "sas-viya-keyvault" {
+  name                = "${var.prefix}-kv-sasviya"
+  resource_group_name = var.resource_group_name
+}
 
 #endregion Azure AD
